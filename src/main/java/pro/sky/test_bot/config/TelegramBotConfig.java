5934ef513ec05ad3c2f925e90bef46b690a94037
Rumonz <lone_assassin@mail.ru>
2023-07-08 00:24:30 +0300

    v1

---
+++ src/main/java/pro/sky/test_bot/config/TelegramBotConfig.java
@@ -0,0 +1,15 @@
+package pro.sky.test_bot.config;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfig {
+
+    @Bean
+    public TelegramBot telegramBot(@Value("${telegram.bot.token}")String token) {
+        return new TelegramBot(token);
+    }
+}
---
+++ src/test/java/pro/sky/test_bot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.test_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
