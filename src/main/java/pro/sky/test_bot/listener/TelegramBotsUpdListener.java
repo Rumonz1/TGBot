607f5ac58e2fea83e2cf1d4c10f1fefa7d584a5f
Rumonz <lone_assassin@mail.ru>
2023-07-13 13:39:17 +0300

    v2

--- src/main/java/pro/sky/test_bot/listener/TelegramBotsUpdListener.java
+++ src/main/java/pro/sky/test_bot/listener/TelegramBotsUpdListener.java
@@ -6,15 +6,26 @@ import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.Nullable;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class TelegramBotsUpdListener implements UpdatesListener {
     private final Logger logger = LoggerFactory.getLogger(TelegramBotsUpdListener.class);
+    private final Pattern pattern = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}:\\d{2})\\s+([А-я\\d\\s.,!?:;]+)");
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
     private final TelegramBot telegramBot;
 
     public TelegramBotsUpdListener(TelegramBot telegramBot) {
@@ -36,12 +47,26 @@ public class TelegramBotsUpdListener implements UpdatesListener {
                 String text = message.text();
 
                 if ("/start".equals(text)) {
-                    SendMessage sendMessage = new SendMessage(chatId, "Чупапимуняню");
-                    SendResponse sendResponse = telegramBot.execute(sendMessage);
-                    if (!sendResponse.isOk()) {
-                        logger.error("Error during sending message: {}", sendResponse.description());
-                    }
+                   sendMessage(chatId, """
+                            Привет!
+                            Я бот-помощник! Помогу запланировать задачу. 
+                            Отправь её в формате: ДД.ММ.ГГГГ ЧЧ:ММ Пример задачи
+                            """);
 
+                } else if (text != null) {
+                    Matcher matcher = pattern.matcher(text);
+                    if (matcher.find()) {
+                        LocalDateTime dateTime = parse(matcher.group(1));
+                        if (Objects.isNull(dateTime)) {
+                            sendMessage(chatId, "Некорректный формат даты и/или времени!");
+                        }else{
+                        String txt = matcher.group(2);
+                        }
+
+                    }else {
+                        sendMessage(chatId, "Некорректный формат сообщения!");
+
+                    }
                 }
 
 
@@ -50,5 +75,22 @@ public class TelegramBotsUpdListener implements UpdatesListener {
             logger.error(e.getMessage(),e);
         }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+@Nullable
+    private LocalDateTime parse(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    private void sendMessage(Long chatId, String message) {
+        SendMessage sendMessage = new SendMessage(chatId, message);
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error during sending message: {}", sendResponse.description());
+        }
+
     }
 }
